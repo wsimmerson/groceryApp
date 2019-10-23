a8f20b8c55d193a81a322ff473c15ff58ba929b3
Wayne Simmerson <wsimmerson@gmail.com>
2019-10-23 09:50:54 -0400

    Grocery Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import {FormsModule} from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/grocery.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GroceryService {
+
+  constructor(
+    private firestore: AngularFirestore
+  ) { }
+
+  create_NewGroceryItem(record) {
+    return this.firestore.collection('Groceries').add(record);
+  }
+
+  read_Groceries() {
+    return this.firestore.collection('Groceries').snapshotChanges();
+  }
+
+  update_GroceryItem(recordID, record) {
+    this.firestore.doc('Groceries/' + recordID).update(record);
+  }
+
+  delete_GroceryItem(recordID) {
+    this.firestore.doc('Groceries/' + recordID).delete();
+  }
+}
